e2d00cc6ca17309c9adc6699b42efc4c3e9aab31
Jack Kelly <Carnificis@users.noreply.github.com>
2023-02-14 19:47:08 -0600

    function

--- utils/content.js
+++ utils/content.js
@@ -1,8 +1,55 @@
 function content(data){
     console.log(data)
     return `
-# Project:${data.title}
+# Project: ${data.title}
+${renderBadge(data.license)}
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Contributor](#contributor)
+4. [Testing](#testing)
+5. [Usage](#usage)
+6. [Github](#github)
+7. [Email](#email)
+8. [License](#license)
+
+
+
+### Description
+${data.description}
+
+### Installation
+${data.installation}
+
+### Contributor
+${data.contributor}
+
+### Testing
+${data.testing}
+$
+### Usage
+${data.usage}
+
+### Github
+${data.github}
+
+### Email
+${data.email}
+
+### License
+${data.license}
+${renderLicenseInfo()}
 
 `
+
+}
+
+function renderBadge(license){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
+
+function renderLicenseInfo(){
+    return `[License Info](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository)`
 }
 module.exports = content
